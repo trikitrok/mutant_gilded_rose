17a75f660ff77b7b4cf49d9d0b596c1a974e2a7f
trikitrok <trikitrok@gmail.com>
2023-06-28 14:13:23 +0200

    canary test

--- js/test/shop.test.js
+++ js/test/shop.test.js
@@ -2,12 +2,9 @@ import {Shop} from '../src/Shop';
 import {Item} from "../src/Item";
 
 describe('Gilded Rose', () => {
-    it('fix me', () => {
-        const items = [new Item('foo', 0, 0)];
-        const shop = new Shop(items);
-
-        shop.update();
-
-        expect(items[0].name).toBe('lala');
-    });
-});
+        it("fix this", () => {
+            expect(1).toEqual(3)
+        });
+    }
+)
+;
